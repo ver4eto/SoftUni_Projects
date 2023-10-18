685fb278f57c71f15f36406882b21fb23b2ce145
ver4eto <vera.ruseva@gmail.com>
2023-10-18 18:15:12 +0300

    New skeleton for Vending machine added

---
+++ source/repos/AdvancedExamPreparation/VendingSystem_Skeleton_netcoreapp6.0/VendingSystem/Drink.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class Drink
+    {
+
+        private string name;
+        private decimal price;
+        private int volume;
+
+
+        public Drink(string name, decimal price, int volume)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Volume = volume;
+        }
+        public  string Name {  get { return name; } set { this.name = value; } }
+        public decimal Price { get { return price; } set { this.price = value; } }
+        public int Volume { get { return volume; } set { this.volume = value; } }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Price: ${Price}, Volume: {Volume} ml";
+        }
+    }
+}
---
+++ source/repos/AdvancedExamPreparation/VendingSystem_Skeleton_netcoreapp6.0/VendingSystem/VendingMachine.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VendingSystem
+{
+    public class VendingMachine
+    {
+        private int buttonCapacity;
+        private List<Drink> drinks;
+
+
+        public VendingMachine(int capacity)
+        {
+            this.ButtonCapacity = capacity;
+            this.Drinks = new List<Drink>();
+        }
+        public int ButtonCapacity {  get { return buttonCapacity; }  private set {  buttonCapacity = value; } }
+        public List<Drink> Drinks { get {  return drinks; } private set {  drinks = value; } }
+
+        public int GetCount => drinks.Count;        
+
+        public void AddDrink(Drink drink)
+        {
+            int currentCount = this.GetCount;
+            if (currentCount < this.ButtonCapacity)
+            {
+                this.Drinks.Add(drink);
+            }
+        }
+
+        public bool RemoveDrink(string name)
+        {
+            Drink currentDrink = this.drinks.FirstOrDefault(d=>d.Name == name);
+            if (currentDrink != null)
+            {
+                this.drinks.Remove(currentDrink);
+                return true;
+            }
+            return false;
+        }
+
+        public Drink GetLongest()
+        {
+            Drink longest=this.drinks.MaxBy(d=>d.Volume);
+            return longest;
+        }
+
+        public Drink GetCheapest()
+        {
+            Drink drink = this.drinks.MinBy(d=>d.Price);
+            return drink;
+        }
+
+        public string BuyDrink(string name)
+        {
+            Drink current = this.drinks.FirstOrDefault(dr => dr.Name == name);
+            return current.ToString();
+        }
+
+        public string Report()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Drinks available:");
+            foreach (var drink in this.drinks)
+            {
+                stringBuilder.AppendLine(drink.ToString());
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
